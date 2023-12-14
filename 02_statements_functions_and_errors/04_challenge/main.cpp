e58fb306dc9ce8082a47d2cce223e53d9da82d0c
yuko-izumi <yuko.izumi@woven-planet.global>
2023-12-14 18:32:24 +0900

    module1-02-04_challenge

---
+++ 02_statements_functions_and_errors/04_challenge/main.cpp
@@ -0,0 +1,30 @@
+// ユーザーが入力した2つの数値のうち大きいほうの値を求めるプログラムがありますが、正しく動作しません。デバッグして修正してください。
+
+#include <iostream>
+
+int main()
+{
+    int num1;
+    int num2;
+    int max;
+
+    std::cout << "Enter two numbers: ";
+    std::cin >> num1 >> num2;
+
+    if (num1 > num2)
+    {
+        max = num1;
+    }
+    else if (num1 < num2)
+    {
+        max = num2;
+    }
+    else
+    {
+        max = num1;
+    }
+
+    std::cout << "Maximum is: " << max << std::endl;
+
+    return 0;
+}
